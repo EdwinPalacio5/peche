37b34dd244543088afc3e1e1e89fe7c389bf6298
Edwin Joel Amaya Palacios <edwin@viasql.com>
2023-06-11 20:11:11 -0600

    new main menu

--- app/src/main/java/com/ues/gpo7fb16014/MainActivity.kt
+++ app/src/main/java/com/ues/gpo7fb16014/MainActivity.kt
@@ -24,24 +24,38 @@ class MainActivity : AppCompatActivity() {
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, menu)
         binding?.let { binding ->
-            binding.listMenu.adapter = arrayAdapter
-            binding.listMenu.setOnItemClickListener { _, _, position, _ ->
-                if (position < 4) {
-                    val nombreValue = activities[position]
-                    try {
-                        val clase = Class.forName("com.ues.gpo7fb16014.crud.$nombreValue")
-                        val inte = Intent(this, clase)
-                        this.startActivity(inte)
-                    } catch (e: ClassNotFoundException) {
-                        e.printStackTrace()
-                    }
-                }else{
-                    val result = controlDB.llenarBD()
-                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
-                }
-            }
+//            binding.listMenu.adapter = arrayAdapter
+//            binding.listMenu.setOnItemClickListener { _, _, position, _ ->
+//                if (position < 4) {
+//                    val nombreValue = activities[position]
+//                    try {
+//                        val clase = Class.forName("com.ues.gpo7fb16014.crud.$nombreValue")
+//                        val inte = Intent(this, clase)
+//                        this.startActivity(inte)
+//                    } catch (e: ClassNotFoundException) {
+//                        e.printStackTrace()
+//                    }
+//                }else{
+//                    val result = controlDB.llenarBD()
+//                    Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+//                }
+//            }
+            binding.btnAlumnos.setOnClickListener { launchActivity(activities[0]) }
+            binding.btnDocentes.setOnClickListener { launchActivity(activities[1]) }
+            binding.btnMaterias.setOnClickListener { launchActivity(activities[2]) }
+            binding.btnRevision.setOnClickListener { launchActivity(activities[3])  }
         }
 
         setContentView(binding!!.root)
+    }
+
+    fun launchActivity(nombreValue : String){
+        try {
+            val clase = Class.forName("com.ues.gpo7fb16014.crud.$nombreValue")
+            val inte = Intent(this, clase)
+            this.startActivity(inte)
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+        }
     }
 }
